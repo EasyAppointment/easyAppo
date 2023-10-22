e9aa1e1291ab343e86180f37e79ef21a10f9973b
ABHAYSAGAR011 <abhaysagar011@gmail.com>
2023-10-22 12:03:53 +0530

    update

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors());
+const userRoute = require("./routes/userRoute.js")
 
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+
+app.use(express.json())
+
+
 const dbConfig = require("./config/dbConfig.js")
-console.log(process.env.MONGO_URL)
+//apis
+app.use('/api/user', userRoute)
+
 app.listen(port , () => console.log("Server Fine"))
---
+++ routes/userRoute.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../models/userModels");
+const bcrypt = require('bcrypt')
+
+router.post('/register' , async(req, res) =>{
+    
+    
+    try{
+
+        const userExists = await User.findOne({email:req.body.email});
+        if(userExists){
+            return res.status(400).send({message: "User already exists", sucess:false})
+        }
+
+        const password = req.body.password;
+        console.log(req.body);
+        console.log("abhay")
+        
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+      
+       
+    //    const newUser = new User(req.body);
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword  // Store the hashed password
+    });
+
+       await newUser.save();
+       res.status(200).send({message:"User created sucessfully"})
+
+    }catch(error){
+       res.status(500).send({message:"Error creating user",sucess:false})
+       console.log(error);
+    }
+})
+
+router.post('/login' , async(req, res) =>{
+    try{
+
+
+    }catch(error){
+        
+    }
+})
+
+module.exports = router;
