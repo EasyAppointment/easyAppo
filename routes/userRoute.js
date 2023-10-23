f10e881e9c8cbd931cd27b8a14a08171c07621b4
EasyAppointment <easyappointment970@gmail.com>
2023-10-23 11:41:34 +0530

    Comp

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -35,7 +35,7 @@ router.post('/register' , async(req, res) =>{
     });
 
        await newUser.save();
-       res.status(200).send({message:"User created sucessfully"})
+       res.status(200).send({message:"User created sucessfully" , sucess:true})
 
     }catch(error){
        res.status(500).send({message: error,sucess:false})
